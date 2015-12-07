3bc84c0865e3d2f9ca74e69da2dd3e88949edf5f
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2015-12-07 11:21:00 -0200

    site up

---
+++ js/main_controller.js
@@ -0,0 +1,24 @@
+APP.controller('MainController', function($scope){
+	$scope.menu = "WEBAPP";
+	$scope.submenu = "TODOS";
+	$scope.showMenu = false;
+
+	$scope.openMenu = function (){
+		$scope.showMenu = !$scope.showMenu;
+	}
+
+	$scope.openSubmenu = function (){
+		$scope.showSubmenu = !$scope.showSubmenu;
+	}
+
+	$scope.selectMenu = function (m){
+		$scope.menu = m;
+		$scope.showMenu = false;
+	}
+
+	$scope.selectSubmenu = function (m){
+		$scope.submenu = m;
+		$scope.showSubmenu = false;
+	}
+
+});
